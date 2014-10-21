3744cc43d0bbf620ac759f38e05945269acf184b
LeonBondeLarsen <lelar09@student.sdu.dk>
2014-10-21 19:02:21 +0200

    Type error...

--- fmApp/sdu/group6/sdu_frobitpro_factory/setup.py
+++ fmApp/sdu/group6/sdu_frobitpro_factory/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-  scripts = ['mission_planners/mission.py', 'mission_planners/topic_controlled_mission.py', 'scripts/qui_node.py'],
+  scripts = ['mission_planners/mission.py', 'mission_planners/topic_controlled_mission.py', 'scripts/gui_node.py'],
   packages = ['rsd_smach'],
   package_dir = {'': 'src'}
 )
